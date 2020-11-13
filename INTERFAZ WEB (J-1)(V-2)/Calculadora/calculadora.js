6fd38c222d42436835dac4fd4e9749102cf15dc4
alberrtooop <alberrto.alg@gmail.com>
2020-11-13 14:27:12 +0100

    13 de octubre actualizacion

--- INTERFAZ WEB (J-1)(V-2)/Calculadora/calculadora.js
+++ INTERFAZ WEB (J-1)(V-2)/Calculadora/calculadora.js
@@ -3,20 +3,56 @@ var operador;
 var numero;
 
 
-function SacarNumero(numero) {
-    var caja2 = document.calculadora.txtcaja.value;
 
-    //var resultado = Mostar();
-    if (caja2 == 0 || isNaN(numero)) {
-        document.calculadora.txtcaja.value = numero + caja2 ;
+
+
+function SacarNumero(numero) {
+    var caja = document.calculadora.txtcaja.value;
+    var caja2 = document.calculadora.txtcaja2.value;
+    //var resultado = Mostar();  || isNaN(numero)
+    if (numero === "+" || numero === "-" || numero === "*" || numero === "/") {
+        document.calculadora.txtcaja2.value = caja.concat(numero);
+        document.calculadora.txtcaja.value = caja + numero;
+
     } else {
-        document.calculadora.txtcaja.value = caja2 + numero;
+        if (caja !== "0") {
+            document.calculadora.txtcaja.value = numero;
+            document.calculadora.txtcaja2.value = caja2.concat(numero);
+        }
+
+        else {
+            if (caja.charAt(0) !== "0") {
+                document.calculadora.txtcaja.value = numero;
+                document.calculadora.txtcaja2.value = caja2.concat(numero);
+            }
+        }
+    }
+
+}
+function Coma() {
+    var caja = document.calculadora.txtcaja.value;
+
+    if (caja.includes('.') < 1) {
+        document.calculadora.txtcaja.value = caja + ".";
+        document.calculadora.txtcaja2.value = caja + ".";
     }
 }
 
+function MasMenos() {
+    var caja = document.calculadora.txtcaja.value;
+
+    if ((caja === "-") < 1) {
+        document.calculadora.txtcaja.value *= -1;
+        document.calculadora.txtcaja2.value *= -1;
+    }
+}
+
+
 function Igual() {
-    document.calculadora.txtcaja.value = eval(document.calculadora.txtcaja.value);
+    let Resultado = document.calculadora.txtcaja2.value;
+    document.calculadora.txtcaja.value = eval(Resultado);
 }
 function Limpiar() {
-    document.calculadora.txtcaja.value = 0;
+    document.calculadora.txtcaja2.value = "";
+    document.calculadora.txtcaja.value = "0";
 }
