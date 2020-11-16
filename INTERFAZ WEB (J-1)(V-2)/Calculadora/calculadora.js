6fc9e0a784be1f186718210a2bfe34e736b4f334
alberrtooop <alberrto.alg@gmail.com>
2020-11-16 14:30:06 +0100

    Añadido 16 de noviembre

--- INTERFAZ WEB (J-1)(V-2)/Calculadora/calculadora.js
+++ INTERFAZ WEB (J-1)(V-2)/Calculadora/calculadora.js
@@ -1,56 +1,72 @@
 var numeroGuardar;
 var operador;
 var numero;
+var nuevo = false;
 
-
-
-
-
+window.open("Calculadora.html","ventana1","width=400,height=400,resizable=no");
 function SacarNumero(numero) {
     var caja = document.calculadora.txtcaja.value;
     var caja2 = document.calculadora.txtcaja2.value;
-    //var resultado = Mostar();  || isNaN(numero)
-    if (numero === "+" || numero === "-" || numero === "*" || numero === "/") {
-        document.calculadora.txtcaja2.value = caja.concat(numero);
-        document.calculadora.txtcaja.value = caja + numero;
 
+    if (caja === "0") {
+        document.calculadora.txtcaja.value = numero;
+        document.calculadora.txtcaja2.value = numero;
     } else {
-        if (caja !== "0") {
-            document.calculadora.txtcaja.value = numero;
+        if (nuevo === true) {
+            if (numero === "+" || numero === "-" || numero === "*" || numero === "/") {
+                document.calculadora.txtcaja.value = caja + numero;
+                document.calculadora.txtcaja2.value = caja2.concat(numero);
+            } else {
+                document.calculadora.txtcaja.value = numero;
+                document.calculadora.txtcaja2.value = numero;
+            }
+            nuevo = false;
+        } else {
+            document.calculadora.txtcaja.value = caja + numero;
             document.calculadora.txtcaja2.value = caja2.concat(numero);
         }
 
-        else {
-            if (caja.charAt(0) !== "0") {
-                document.calculadora.txtcaja.value = numero;
-                document.calculadora.txtcaja2.value = caja2.concat(numero);
-            }
-        }
     }
-
 }
+
+function backSpace() {
+    var value = document.getElementById("txtcaja").value;
+    if (value.substr(0, value.length - 1) === "") {
+        document.getElementById("txtcaja").value = "0";
+        document.getElementById("txtcaja2").value = "";
+    } else {
+        document.getElementById("txtcaja").value = value.substr(0, value.length - 1);
+        document.getElementById("txtcaja2").value = value.substr(0, value.length - 1);
+    }
+}
+
+
 function Coma() {
     var caja = document.calculadora.txtcaja.value;
-
-    if (caja.includes('.') < 1) {
-        document.calculadora.txtcaja.value = caja + ".";
-        document.calculadora.txtcaja2.value = caja + ".";
-    }
+    document.calculadora.txtcaja.value = caja + ".";
+    document.calculadora.txtcaja2.value = caja + ".";
 }
 
 function MasMenos() {
-    var caja = document.calculadora.txtcaja.value;
-
-    if ((caja === "-") < 1) {
-        document.calculadora.txtcaja.value *= -1;
-        document.calculadora.txtcaja2.value *= -1;
-    }
+    document.calculadora.txtcaja.value *= -1;
+    document.calculadora.txtcaja2.value *= -1;
 }
 
-
 function Igual() {
-    let Resultado = document.calculadora.txtcaja2.value;
-    document.calculadora.txtcaja.value = eval(Resultado);
+    var Resultado = document.calculadora.txtcaja.value;
+    var Resultado2 = document.calculadora.txtcaja2.value;
+    try {
+        if (isNaN(eval(Resultado)) || Resultado.includes('//')) {
+            document.calculadora.txtcaja.value = "SyntaxError";
+        } else {
+            document.calculadora.txtcaja.value = eval(Resultado2);
+            nuevo = true;
+        }
+
+    } catch (e) {
+        document.calculadora.txtcaja.value = "SyntaxError";
+    }
+
 }
 function Limpiar() {
     document.calculadora.txtcaja2.value = "";
