e1b64890425e34b5d25a7b6f62a3d422214de0aa
alberrtooop <alberrto.alg@gmail.com>
2021-01-14 01:36:29 +0100

    14 de enero 2021

--- APLICACIONES WEB (L)/Tarea1/TareaValidar1_AlbertoMoyano.js
+++ APLICACIONES WEB (L)/Tarea1/TareaValidar1_AlbertoMoyano.js
@@ -1,16 +1,32 @@
-function Rango() {
-    var txt = "";
-    if (document.getElementById("NumeroValido").validity.rangeOverflow) {
-        txt = "Value too large";
-    } else if (document.getElementById("NumeroValido").validity.rangeUnderflow) {
-        txt = "Input OK";
+$(document).ready(function () {
+    $("#butonPrimo").click(function () {
+        var numeroValor = $("#NumeroValido").val();
+        var primo = true;
+
+        if (numeroValor <= 1) {
+            primo = false
+        }
+        for (let index = 2; index < numeroValor; index++) {
+            if (numeroValor % index == 0) {
+                primo = false
+            }
+        }
+        if (primo === false) {
+            return $("#CajaContenido").append("<p> No es Primo </p>");
+        }
+        else {
+            return $("#CajaContenido").append("<p>" + numeroValor + "</p>");
+        }
+    });
+});
+
+function Valido() {
+    var numeroValor = document.getElementById("NumeroValido");
+    if (!numeroValor.checkValidity()) {
+        document.getElementById("Valido").innerHTML = "<p>" + numeroValor.validationMessage + " </p>";
+    } else {
+        document.getElementById("Valido").innerHTML = "<p> Ok </p>";
     }
-    document.getElementById("CajaContenido").innerHTML = txt;
 }
 
 
-
-
-function EsNumeroPrimo(params) {
-
-}
